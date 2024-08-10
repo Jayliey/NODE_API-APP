082de0b51fe1278475ce96e4961248e0c29b372c
Juliet Chirimumimba <chirimumimbajuliet@gmail.com>
2024-08-10 13:55:16 +0200

    phase 2

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const mongoose =require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //Routes
 
@@ -7,7 +12,81 @@ app.get('/', (req, res) => {
     res.send('Hello Node API')
 })
 
+app.get('/blog', (req, res) => {
+    res.send('Hello blog, My name is Juliet')
+})
 
-app.listen(3000, ()=> {
-    console.log('Node API app is running on port 3000')
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+//update a product
+
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(404).json({message: 'cannot find product with ID ${id}'})
+        }
+        const updatedProduct =  await Product.findById(id);
+        res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({message: error.message}) 
+    }
+})
+
+// delete a product
+
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: 'cannot find a product with ID ${id}'})
+        }
+        res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+mongoose.set('strictQuery', false)
+mongoose.
+connect('mongodb+srv://admin:1234567JC@julietapi.xaxapnu.mongodb.net/NODE_API')
+.then(() => {
+    console.log('connected to mongodb')
+    app.listen(3000, ()=> {
+        console.log('Node API app is running on port 3000')
+    })
+    
+}).catch((error) => {
+    console.log(error)
 })
